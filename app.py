5ef8d2a0e1c64e3e7b74a08245fc65ddd4b33669
Solomon Malih <smalih15@hotmail.com>
2023-08-15 12:55:43 +0100

    commented out gen_file as redundant

--- app.py
+++ app.py
@@ -145,20 +145,20 @@ def decoded():
     hidden_message = session.get('hidden_message')
     return render_template('decoded.html', hidden_message=hidden_message)
 
-@app.route('/genfile', methods=['GET'])
-def gen_file():
+# @app.route('/genfile', methods=['GET'])
+# def gen_file():
 
     
-    categories='nature, city, technology, food, still_life, abstract, wildlife'.split(", ")
+#     categories='nature, city, technology, food, still_life, abstract, wildlife'.split(", ")
 
-    api_url = 'https://api.api-ninjas.com/v1/randomimage?category={}'.format(category)
-    response = requests.get(api_url, headers={'X-Api-Key': 'vUkWtBsXjrU12mz7Ep8YdQ==TYN8vUz4sZ34Rfe2', 'Accept': 'image/jpg'}, stream=True)
-    if response.status_code == requests.codes.ok:
-        with open('img.jpg', 'wb') as out_file:
-            shutil.copyfileobj(response.raw, out_file)
-    else:
-        print("Error:", response.status_code, response.text)
-    return redirect(url_for('upload_file'))
+#     api_url = 'https://api.api-ninjas.com/v1/randomimage?category={}'.format(category)
+#     response = requests.get(api_url, headers={'X-Api-Key': 'vUkWtBsXjrU12mz7Ep8YdQ==TYN8vUz4sZ34Rfe2', 'Accept': 'image/jpg'}, stream=True)
+#     if response.status_code == requests.codes.ok:
+#         with open('img.jpg', 'wb') as out_file:
+#             shutil.copyfileobj(response.raw, out_file)
+#     else:
+#         print("Error:", response.status_code, response.text)
+#     return redirect(url_for('upload_file'))
 
 # @app.route('/download/<name>', methods=["POST"])
 # def download(name):
